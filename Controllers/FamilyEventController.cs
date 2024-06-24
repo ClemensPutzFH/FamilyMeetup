d60d12226520ef6ccf08d95843cb9c121df80727
alexvujisic <alexandar.vujisic@stud.fh-campuswien.ac.at>
2024-06-24 23:33:16 +0200

    implemented function for alphanumeric check

--- Controllers/FamilyEventController.cs
+++ Controllers/FamilyEventController.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Family_Meetup.FamilyEvents;
 using Family_Meetup.Models;
 using Family_Meetup.Models.Services;
@@ -26,15 +27,21 @@ namespace Family_Meetup.Controllers
 
 
         [HttpPut("/CreateEvent")]
-        [ProducesResponseType(StatusCodes.Status201Created, Type=typeof(CreateEventResponse))]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CreateEventResponse))]
         public IActionResult CreateEvent(CreateEventRequest request)
         {
 
+            //if title, creator or location is not alphanumeric return badrequest
+            if (!IsAlphanumeric(request.Title) || !IsAlphanumeric(request.Creator) || !IsAlphanumeric(request.Location))
+            {
+                return BadRequest("Only alphanumeric characters are allowed for Title, Creator, and Location.");
+            }
+
             var dateVoteOptions = new List<MeetupDateVoteOption>();
 
-           foreach(DateTime dateOption in request.MeetupDateVoteOptions)
+            foreach (DateTime dateOption in request.MeetupDateVoteOptions)
             {
-                dateVoteOptions.Add(new MeetupDateVoteOption(dateOption,new List<string>(), Guid.NewGuid()));
+                dateVoteOptions.Add(new MeetupDateVoteOption(dateOption, new List<string>(), Guid.NewGuid()));
             }
 
             var familyEvent = new Event(
@@ -54,7 +61,7 @@ namespace Family_Meetup.Controllers
 
 
             return CreatedAtAction(
-                nameof(CreateEvent), 
+                nameof(CreateEvent),
                 new CreateEventResponse(Location: "/GetEvent/" + familyEvent.id.ToString()));
         }
 
@@ -68,7 +75,7 @@ namespace Family_Meetup.Controllers
         }
 
         [HttpGet("/GetAllEvents")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(IEnumerable<EventShort>))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<EventShort>))]
         public IActionResult GetAllEvent(string username, string searchUsername)
         {
             List<Event> familyEvents = _familyEventService.getAllEvents(username, searchUsername);
@@ -82,11 +89,18 @@ namespace Family_Meetup.Controllers
         }
 
 
+
         [HttpPost]
         [Route("/VoteDate/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult voteDate(Guid id, string username, [FromBody] VoteDateRequest voteDateRequest)
         {
+            //if username is not alphanumeric return badrequest
+            if (!IsAlphanumeric(username))
+            {
+                return BadRequest("Only alphanumeric characters are allowed for Username.");
+            }
+
             String respond = _familyEventService.voteDate(id, username, voteDateRequest);
             return Ok(respond);
         }
@@ -96,8 +110,28 @@ namespace Family_Meetup.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult addComment(Guid id, string username, [FromBody] CommentRequest commentRequest)
         {
+
+            //if username or comment is not alphanumeric return badrequest
+            if (!IsAlphanumeric(username) || !IsAlphanumericWithPunctuation(commentRequest.Comment))
+            {
+                return BadRequest("Only alphanumeric characters are allowed for Username and comment.");
+            }
+
             String respond = _familyEventService.addComment(id, username, commentRequest.Comment);
             return Ok(respond);
+        }
+
+
+        //function to check if request is alphanumeric 
+        private bool IsAlphanumeric(string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z0-9]*$");
+        }
+
+        //function to check if request is alphanumeric with exception punctuation
+        private bool IsAlphanumericWithPunctuation(string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z0-9.,?! ]*$");
         }
 
         /*
